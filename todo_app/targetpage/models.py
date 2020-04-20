19493444a2b4497839867803fcfe1de54fc37432
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-20 16:28:44 +0900

    [add] models作成(target)

--- todo_app/targetpage/models.py
+++ todo_app/targetpage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Target(models.Model):
+    target_name = models.CharField('行動目標名', max_length=255)
+    target_content = models.CharField('内容', max_length=255)
+    target_status = models.IntegerField('学習ステータス')
+    expectation_time = models.IntegerField('予想時間')
+    actual_time = models.IntegerField('実際時間')
+    understanding_level = models.IntegerField('理解度')
+    objective = models.ForeignKey(
+        'objectivepage.Objective',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.target_name
+
+    class Meta:
+        db_table = 'target'
+        verbose_name = '行動目標名'
+        verbose_name_plural = '行動目標名'
