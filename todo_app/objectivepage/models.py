f378815612d88ecf31613778c1e245b240f037a7
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-20 16:28:53 +0900

    [add] models作成(objective)

--- todo_app/objectivepage/models.py
+++ todo_app/objectivepage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Objective(models.Model):
+    object_name = models.CharField('目標名', max_length=255)
+    object_category = models.CharField('カテゴリ', max_length=255)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.object_name
+
+    class Meta:
+        db_table = 'objective'
+        verbose_name = '目標名'
+        verbose_name_plural = '目標名'
