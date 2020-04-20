e4690700a86ec86cc686822ea357b76d80bdaa0b
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-20 16:20:15 +0900

    [fix] 実装できなかったので元に戻す

--- todo_app/accounts/urls.py
+++ todo_app/accounts/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-# https://docs.djangoproject.com/en/2.0/intro/tutorial03/
 app_name = 'accounts'
 
 urlpatterns = [
     # ex: /accounts/signup/
-    path('/', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
