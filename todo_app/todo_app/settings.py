6264a3c6137097a78f2f896b0a24dc0b0964f379
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-20 14:50:52 +0900

    [add] 作成したアプリケーションをsettingに登録

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'objectivepage.apps.ObjectivepageConfig',
+    'targetpage.apps.TargetpageConfig',
 ]
 
 MIDDLEWARE = [
