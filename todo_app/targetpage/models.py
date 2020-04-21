5dc402faa430c39fb25e460313d5f8585833f6a5
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 10:29:32 +0900

    [fix] データ型を数値から時間を扱うフィールドに変更

--- todo_app/targetpage/models.py
+++ todo_app/targetpage/models.py
@@ -6,8 +6,8 @@ class Target(models.Model):
     target_content = models.CharField('内容', max_length=255)
     target_status = models.IntegerField('学習ステータス')
     expectation_time = models.IntegerField('予想時間')
-    actual_time = models.IntegerField('実際時間')
-    understanding_level = models.IntegerField('理解度')
+    actual_time = models.DurationField('実際時間')
+    understanding_level = models.DurationField('理解度')
     objective = models.ForeignKey(
         'objectivepage.Objective',
         on_delete=models.CASCADE
