b6b42da9486a00e424cb08f684c437560a55b7cb
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-22 01:48:39 +0900

    [add] 行動目標登録処理実装

--- todo_app/targetpage/views.py
+++ todo_app/targetpage/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from targetpage.models import Target
+from targetpage.forms import TargetForm
+from pprint import pprint
 
 # Create your views here.
+def create_target(request):
+    form = TargetForm(request.POST or None)
+    print(form.errors)
+    if form.is_valid():
+        form.save()
+        # ここにリダイレクト処理を追記する（トップへ）まだ、リダイレクト先が無いので未入力
+
+    return render(request, 'target/create_form.html',{
+        'form': form,
+    })
