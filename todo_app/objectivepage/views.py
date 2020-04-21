2a58cd74310c0ebe773e85d3c35ee25126e5fe74
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 15:34:39 +0900

    [add] save()の簡略化

--- todo_app/objectivepage/views.py
+++ todo_app/objectivepage/views.py
@@ -6,11 +6,7 @@ from objectivepage.forms import ObjectiveForm
 def add_object(request):
     form = ObjectiveForm(request.POST or None, initial={'user': request.user})
     if form.is_valid():
-        objective = Objective()
-        objective.object_name = form.cleaned_data['object_name']
-        objective.object_category = form.cleaned_data['object_category']
-        objective.user = form.cleaned_data['user']
-        objective.save()
+        form.save()
         pass
         # ここにリダイレクト処理を追記する（トップへ）まだ、リダイレクト先が無いので未入力
 
