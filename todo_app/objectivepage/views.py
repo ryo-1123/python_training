9a49fb66b492280391f0678cffd74eb796d01469
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 14:19:57 +0900

    [add] 送信されたリクエストを保存,保存後のリダイレクト先はまだ

--- todo_app/objectivepage/views.py
+++ todo_app/objectivepage/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from objectivepage.models import Objective
+from objectivepage.forms import ObjectiveForm
 
 # Create your views here.
+def add_object(request):
+    form = ObjectiveForm(request.POST or None)
+    if form.is_valid():
+        objective = Objective()
+        objective.object_name = form.cleaned_data['object_name']
+        objective.object_category = form.cleaned_data['object_category']
+        objective.user = request.user
+        objective.save()
+        pass
+        # ここにリダイレクト処理を追記する（トップへ）まだ、リダイレクト先が無いので未入力
+
+    return render(request, 'objective/object_form.html',{
+        'form': form,
+    })
