0324c613dbe1dc2dc3cab2c92ebf48e0b428be76
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 15:31:22 +0900

    [update] userの初期化

--- todo_app/objectivepage/views.py
+++ todo_app/objectivepage/views.py
@@ -4,7 +4,7 @@ from objectivepage.forms import ObjectiveForm
 
 # Create your views here.
 def add_object(request):
-    form = ObjectiveForm(request.POST or None)
+    form = ObjectiveForm(request.POST or None, initial={'user': request.user})
     if form.is_valid():
         objective = Objective()
         objective.object_name = form.cleaned_data['object_name']
