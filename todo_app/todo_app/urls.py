f7ef6da63741cfa1fb0d2d1d4ba5af94181c652d
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 15:56:12 +0900

    [add] プロジェクトにtargetアプリケーションのurlを追加

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('objective/', include('objectivepage.urls')),
+    path('target/', include('targetpage.urls')),
 ]
