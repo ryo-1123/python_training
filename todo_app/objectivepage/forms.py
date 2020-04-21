d65aac0167d78be7fdf33a5e12c838f3dcd9390a
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-21 14:03:49 +0900

    [add] formをmodelFormで作成

---
+++ todo_app/objectivepage/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from objectivepage.models import Objective
+
+
+class ObjectiveForm(forms.ModelForm):
+    class Meta:
+        model = Objective
+        fields = [
+            'object_name',
+            'object_category',
+        ]
