906603a666908fc843c526fd68319953fdab63d4
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-22 23:18:05 +0900

    [add] 行動目標の一覧を取得

--- todo_app/listpage/views.py
+++ todo_app/listpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from objectivepage.models import Objective
+from targetpage.models import Target
 
 # Create your views here.
 def display_list(request):
@@ -7,6 +8,13 @@ def display_list(request):
         user=request.user
     )
 
+    targets = {}
+    for num in range(1, 4):
+        targets[num] = Target.objects.filter(
+            target_status=num
+        )
+
     return render(request, 'listpage/index.html', {
         'objectives': objectives,
+        'targets': targets,
     })
