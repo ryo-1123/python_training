8b148787fffc55754f94dc021ebb8ba0bbdf226d
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-22 14:46:49 +0900

    [add] objectiveをログイン情報でfilterを掛けて取得

--- todo_app/listpage/views.py
+++ todo_app/listpage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from objectivepage.models import Objective
 
 # Create your views here.
+def display_list(request):
+    objectives = Objective.objects.filter(
+        user=request.user
+    )
+
+    return render(request, 'listpage/index.html', {
+        'objectives': objectives,
+    })
