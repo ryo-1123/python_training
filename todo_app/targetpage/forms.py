f1c91d237c63201aaefbe3e212155b3ae3d478f7
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-22 14:15:44 +0900

    [fix] importしたdecimalでmin_valueを設定する

--- todo_app/targetpage/forms.py
+++ todo_app/targetpage/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from targetpage.models import Target
 from django.core.exceptions import ValidationError
+import decimal
 
 
 class TargetForm(forms.ModelForm):
     expectation_time = forms.DecimalField(
         label='完了予想時間',
-        min_value=0.1,
+        min_value=decimal.Decimal(0.1),
         )
 
     class Meta:
