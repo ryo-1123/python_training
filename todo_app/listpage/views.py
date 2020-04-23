9cf7e2ec09f87d6a7d019390b9cd25927b8748f1
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-23 11:25:43 +0900

    [add] 新しく作られた順に並び替えました。

--- todo_app/listpage/views.py
+++ todo_app/listpage/views.py
@@ -6,7 +6,7 @@ from targetpage.models import Target
 def display_list(request):
     objectives = Objective.objects.filter(
         user=request.user
-    )
+    ).order_by('id').reverse()
 
     targets = {
         1: [],
@@ -18,7 +18,7 @@ def display_list(request):
             target = Target.objects.filter(
                 target_status=num,
                 objective=objective,
-            )
+            ).order_by('id').reverse()
             targets[num] += list(target)
 
     print(targets)
