be9fe5703e452a8e427d2aa9b1db0846978136de
ryo-1123 <adiinoyr.1617@gmail.com>
2020-04-23 14:32:47 +0900

    [add] 編集用のform作成

--- todo_app/targetpage/forms.py
+++ todo_app/targetpage/forms.py
@@ -18,3 +18,18 @@ class TargetForm(forms.ModelForm):
             'expectation_time',
         ]
 
+
+class TargetEditForm(forms.ModelForm):
+    expectation_time = forms.DecimalField(
+        label='完了予想時間',
+        min_value=decimal.Decimal(0.1),
+    )
+
+    actual_time = forms.DecimalField(
+        label='完了実際時間',
+        min_value=decimal.Decimal(0.1),
+    )
+
+    class Meta:
+        model = Target
+        fields = '__all__'
